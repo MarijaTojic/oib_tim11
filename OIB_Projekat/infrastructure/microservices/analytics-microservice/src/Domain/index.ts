f2bf8ce19a3731a3cf208fb2f9f819f7d7c66ba4
Marija Gajic <mgajic155@gmail.com>
2026-01-21 19:50:33 +0100

    Dodavanje enuma za analitiku

---
+++ OIB_Projekat/infrastructure/microservices/analytics-microservice/src/Domain/index.ts
@@ -0,0 +1,5 @@
+export { Receipt } from './models/Receipt';
+export { ReportAnalysis } from './models/ReportAnalysis';
+export { ReportPerformance } from './models/ReportPerformance';
+export { SaleType } from './enums/SaleType';
+export { PaymentType } from './enums/PaymentType';
